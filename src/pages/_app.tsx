cac990aedbe870610ac47972a0a6ae79d01c506d
Fabio Benedetti <fabio.benedetti@7-tv.de>
2020-10-28 21:33:54 +0100

    add webvitals

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,10 @@ import "react-calendar/dist/Calendar.css";
 import Router from "next/router";
 import "tailwindcss/dist/base.min.css";
 
+export function reportWebVitals(metric) {
+  console.log(metric);
+}
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
