666294b71357bbaa6f17ee4e4c3739ec35266274
Fabio Benedetti <fabio.benedetti@joyn.de>
2020-12-28 12:33:48 +0100

    fix:cls

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const withPWA = require("next-pwa");
 
 module.exports = withPWA({
   target: "serverless",
+  images: {
+    domains: ["a0.muscache.com"],
+  },
   pwa: {
     disable: process.env.NODE_ENV === "development",
     dest: "public",
