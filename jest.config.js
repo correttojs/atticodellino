cc9dd086e155040acc0fdcaf39a3ede9ff3ea7cf
Fabio Benedetti <fabio.benedetti@joyn.de>
2021-01-30 12:53:13 +0100

    feat: add tests

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+process.env.WEBPACK_TOGGLE = {};
+module.exports = {
+  automock: false,
+  setupFiles: ["./setupJest.js"],
+
+  moduleNameMapper: {
+    "^@/(.*)": "<rootDir>/src/$1",
+  },
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "graphql"],
+  modulePathIgnorePatterns: [
+    "<rootDir>/.next",
+    "<rootDir>/coverage/",
+    "<rootDir>/cypress",
+    "<rootDir>/cache",
+    "<rootDir>/build",
+  ],
+  transform: {
+    "\\.(gql|graphql)$": "jest-transform-graphql",
+    "\\.tsx?$": "babel-jest",
+  },
+  globals: {
+    __VERSION__: "test",
+  },
+};
