cecfab3bc66763eee8b07f6367cfd5301bed8dae
Fabio Benedetti <fabio.benedetti@joyn.de>
2021-01-30 13:27:22 +0100

    feat: test

--- src/graphql/__tests__/_calendar.test.ts
+++ src/graphql/__tests__/_calendar.test.ts
@@ -1,10 +1,11 @@
 import fetch from "jest-fetch-mock";
 
-import { fetchIcal } from "../_calendar";
+import { calendarResolver, fetchIcal } from "../_calendar";
+import * as takeshape from "../takeshape";
 
-describe("Calendar", () => {
-  it("fetchIcal", async () => {
-    fetch.mockResponseOnce(`BEGIN:VCALENDAR
+jest.mock("../takeshape");
+
+const calendarMock = `BEGIN:VCALENDAR
 PRODID;X-RICAL-TZSOURCE=TZINFO:-//Airbnb Inc//Hosting Calendar 0.8.8//EN
 CALSCALE:GREGORIAN
 VERSION:2.0
@@ -20,10 +21,13 @@ DTSTART;VALUE=DATE:20210729
 UID:6fec1092d3fa-bbaf28e08fe4dce175501a85999001b8@airbnb.com
 SUMMARY:Airbnb (Not available)
 END:VEVENT
-END:VCALENDAR`);
+END:VCALENDAR`;
+
+describe("Calendar", () => {
+  it("fetchIcal", async () => {
+    fetch.mockResponseOnce(calendarMock);
     const data = await fetchIcal("https://url", "summay");
 
-    expect(data.length).toEqual(2);
     expect(data.length).toEqual(2);
     expect(data).toEqual([
       {
@@ -37,5 +41,33 @@ END:VCALENDAR`);
         summary: "summay",
       },
     ]);
+  });
+
+  it("calendarResolver", async () => {
+    fetch.mockResponse(calendarMock);
+    const takeShapeRequestMock = jest.spyOn(takeshape, "takeShapeRequest");
+    takeShapeRequestMock.mockImplementation(() => {
+      return Promise.resolve({
+        getApartmentList: {
+          items: [
+            {
+              airbnbIcal: "test",
+              bookingIcal: "test",
+            },
+          ],
+        },
+      });
+    });
+    const data = await (calendarResolver as any)(null as any, {
+      apartment: "VR",
+    });
+
+    expect(data.length).toEqual(4);
+    expect(data[0].summary).toEqual("AIRBNB");
+    expect(data[0].start).toEqual("2021-01-27T23:00:00.000Z");
+    expect(data[0].start).toEqual("2021-01-27T23:00:00.000Z");
+    expect(data[0].end).toEqual("2021-04-29T22:00:00.000Z");
+
+    expect(data[3].summary).toEqual("BOOKING");
   });
 });
