93e8d73cb6532575c07c6815eaa9dc400975c786
Fabio Benedetti <fabio.benedetti@joyn.de>
2021-05-03 21:51:38 +0200

    feat test

--- src/hooks/useTranslations/__tests__/useTranslations.test.tsx
+++ src/hooks/useTranslations/__tests__/useTranslations.test.tsx
@@ -1,14 +1,14 @@
-import * as MockGlobal from "@/components/Layout/useGlobal";
 import { renderHook } from "@testing-library/react-hooks";
+import * as MockRouter from "next/router";
 
 import { useTranslations } from "../useTranslations";
 
 describe("useTranslations", () => {
   it("should translate to EN", () => {
-    jest.spyOn(MockGlobal, "useGlobal").mockImplementation(
+    jest.spyOn(MockRouter, "useRouter").mockImplementation(
       () =>
         ({
-          lang: "en",
+          locale: "en",
         } as any)
     );
     const Mock = ({ children }: any) => <div>{children}</div>;
@@ -18,10 +18,10 @@ describe("useTranslations", () => {
     expect(result.current("SPACE")).toEqual("The space");
   });
   it("should translate to IT", () => {
-    jest.spyOn(MockGlobal, "useGlobal").mockImplementation(
+    jest.spyOn(MockRouter, "useRouter").mockImplementation(
       () =>
         ({
-          lang: "it",
+          locale: "it",
         } as any)
     );
     const Mock = ({ children }: any) => <div>{children}</div>;
