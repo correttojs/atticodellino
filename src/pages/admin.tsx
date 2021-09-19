a45f6d536d70415bc01717f114502ae7fcd55e8b
Fabio Benedetti <fabio.benedetti+github@gmail.com>
2021-09-19 12:14:56 +0200

    feat: admin

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -4,7 +4,7 @@ import { getGlobalProps } from "@/graphql/takeshape/getGlobal";
 import { GetStaticProps } from "next";
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
-  const params = { lang: "en", apartment: "VR" };
+  const params = { lang: "en", apartment: "GARDA" };
   const globalProps = await getGlobalProps({ params, locale });
 
   return globalProps ?? { props: {} };
